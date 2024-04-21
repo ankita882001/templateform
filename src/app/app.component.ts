30f19b074ad1bb48c9f71889ae3d596c3e3d43ba
ankita882001 <122888317+ankita882001@users.noreply.github.com>
2024-04-21 16:32:03 +0530

    templateform

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,51 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'templateform';
+  @ViewChild ("signUp") signUp!:NgForm
+  mocArr=[
+    {
+      contactmode:'Email',
+      id:123
+    },
+    {
+      contactmode:'Phone',
+      id:124
+    }
+  ]
+userDetails={
+ "userInfo":{
+    "username":"ankita",
+    "email": "ankitakulkarni012@gmail.com"
+  },
+
+"answer": "Cricket",
+"modeofcontact": "Email",
+"secretque": "sport",
+"subscribe": "true"
+}
+
+constructor(){
+
+}
+  ngOnInit(): void {
+      
+  }
+  onSignUp(signUp:NgForm){
+    if(signUp.valid){
+      console.log(signUp.valid)
+      console.log(signUp);
+    console.log(signUp.value)
+    }
+    
+  }
+  onEdit(){
+    this.signUp.form.patchValue(this.userDetails)
+  }
 }
